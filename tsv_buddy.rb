89723dc1f89730370d4ec94f225d17b38ad7d480
rex5207 <abab5207@gmail.com>
2017-09-30 17:05:59 +0800

    first commit

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -1,14 +1,24 @@
+require 'csv'
+
 # Module that can be included (mixin) to take and output TSV data
 module TsvBuddy
   # take_tsv: converts a String with TSV data into @data
   # parameter: tsv - a String in TSV format
   def take_tsv(tsv)
-
+    @data = []
+    CSV.parse(tsv, col_sep: "\t", headers: true) do |row|
+      @data.push(row.to_hash)
+    end
   end
 
   # to_tsv: converts @data into tsv string
   # returns: String in TSV format
   def to_tsv
-
+    headers = @data.first.keys
+    result = headers.join("\t") + "\n"
+    @data.each do |row|
+      result += row.values.join("\t") + "\n"
+    end
+    result
   end
 end
--- yaml_buddy.rb
+++ yaml_buddy.rb
@@ -1,1 +1,16 @@
-# Module that can be included (mixin) to take and output Yaml data
+require 'yaml'
+
+# Module that can be included (mixin) to take and output YAML data
+module YamlBuddy
+  # take_tsv: converts a String with TSV data into @data
+  # parameter: tsv - a String in TSV format
+  def take_yaml(yaml)
+    @data = YAML.safe_load(yaml)
+  end
+
+  # to_tsv: converts @data into tsv string
+  # returns: String in TSV format
+  def to_yaml
+    @data = @data.to_yaml
+  end
+end
